6463bcb53d5b605f025e7f0ab1ecb7e9685a19db
Thibaut Lunet <thibaut.lunet@tuhh.de>
2025-01-20 10:43:02 +0100

    TL: additional python translations

---
+++ chap1/sec1.3/exampleWave.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameters
+N = 5000  # Number of time steps
+M = 50    # Number of spatial intervals
+L = 1     # Length of the domain
+dx = L / M
+T = 10    # Total simulation time
+dt = T / N
+
+x = np.linspace(0, L, M + 1)  # Spatial grid
+u = np.zeros((M + 1, N + 1))  # Solution array
+
+# Initial conditions
+for j in range(len(x)):
+    if x[j] < L / 2:
+        u[j, 0] = x[j]  # Triangular initial profile
+    else:
+        u[j, 0] = L - x[j]
+    u[j, 1] = u[j, 0]  # Zero initial velocity (ut0 = 0)
+
+# Time-stepping loop
+for n in range(1, N):
+    # Update interior points using finite difference method
+    u[1:-1, n + 1] = (
+        2 * u[1:-1, n]
+        - u[1:-1, n - 1]
+        + (dt**2 / dx**2) * (u[0:-2, n] - 2 * u[1:-1, n] + u[2:, n])
+    )
+    
+    # Plot solution at current time step
+    plt.clf()
+    plt.plot(x, u[:, n], label=f't={(n-1)*dt:.2f}')
+    plt.axis([0, 1, -0.6, 0.6])
+    plt.xlabel('x')
+    plt.ylabel('u')
+    plt.title('Wave Equation Simulation')
+    plt.grid()
+    plt.pause(0.01)  # Pause for animation effect
+
+plt.show()
---
+++ chap1/sec1.4/example2Grid.py
@@ -0,0 +1,59 @@
+import numpy as np
+import scipy.sparse as sp
+import scipy.sparse.linalg as spla
+import matplotlib.pyplot as plt
+
+# Parameters
+l = 4
+J = 2**l - 1  # Fine grid size
+e = np.ones(J)
+h = 1 / (J + 1)
+x = np.linspace(0, 1, J + 2)
+
+# Construct the 1D Laplacian matrix on the fine grid
+A = 1 / h**2 * sp.diags([e, -2 * e, e], [-1, 0, 1], shape=(J, J))
+
+# Coarse grid parameters
+Jc = 2**(l - 1) - 1  # Coarse grid size
+P = sp.lil_matrix((J, Jc))  # Prolongation matrix (interpolation)
+for j in range(1, Jc + 1):
+    P[2 * j - 1, j - 1] = 1
+    if 2 * j - 2 >= 0:
+        P[2 * j - 2, j - 1] = 0.5
+    if 2 * j < J:
+        P[2 * j, j - 1] = 0.5
+P = P.tocsr()
+
+R = 0.5 * P.T  # Restriction matrix
+Ac = R @ A @ P  # Galerkin coarse-grid matrix
+
+# Multigrid parameters
+nu = 2  # Number of smoothing steps
+np.random.seed(0)
+u = np.random.rand(J)  # Random initial guess
+w = 2 / 3  # Jacobi damping parameter
+
+# Multigrid V-cycle
+for n in range(3):  # Number of multigrid iterations
+    for i in range(nu):  # Jacobi smoothing steps
+        u = u + w * h**2 / 2 * A @ u  # Jacobi damping step
+
+    # Plot before coarse correction
+    plt.plot(x, np.concatenate(([0], u, [0])), label="Before coarse correction")
+    plt.xlabel("x")
+    plt.ylabel(f"Error (iteration {n + 1})")
+    plt.axis([0, 1, -0.1, 1])
+    
+    # Compute residual and coarse correction
+    rc = R @ (-A @ u)  # Residual (f = 0)
+    uc = spla.spsolve(Ac, rc)  # Solve coarse problem
+    u = u + P @ uc  # Correct fine-grid solution
+
+    # Plot after coarse correction
+    plt.plot(x, np.concatenate(([0], u, [0])), "r", label="After coarse correction")
+    plt.legend()
+    plt.grid()
+    plt.pause(1.5)
+    plt.clf()
+
+plt.show()
